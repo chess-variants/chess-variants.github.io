923fdc0fdcda160ffdae2328294f6febabbfc94c
Fabian Fichter <ianfab@users.noreply.github.com>
2023-11-05 13:20:20 +0100

    Try to strip zip codes

--- _scripts/update_tournaments.py
+++ _scripts/update_tournaments.py
@@ -34,8 +34,9 @@ def get_fesa_tourneys():
         return datetime.datetime.strptime(row[col].strip(), '%d %b').replace(year=int(row['Year'])).strftime('%Y-%m-%d')
     df['Start'] = df.apply(lambda r: reformat_date(r, 'Start'), axis=1)
     df['End'] = df.apply(lambda r: reformat_date(r, 'End'), axis=1)
-    # remove street name in location
+    # try to remove street names and zip codes in location
     df['Place'].replace(to_replace='[^, ][^,]* \d+,\s*', value='', regex=True, inplace=True)
+    df['Place'].replace(to_replace='\d+(-\d+)? (\w+)', value=r'\2', regex=True, inplace=True)
     # add additional info
     df['Source'] = "<a href='{}'>fesashogi.eu</a>".format(URL)
     df['Variant'] = 'Shogi'
